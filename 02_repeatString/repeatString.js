ac3660be2927707cc16ee586d5fede4facbcd6df
Thiago S. Santos <thiagos.xsantos@gmail.com>
2022-10-15 18:23:08 -0300

    Resolve JS exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,5 @@
-const repeatString = function() {
-
+const repeatString = function(text, count) {
+  return count >= 0 ? (new String(text)).repeat(count) : 'ERROR';
 };
 
 // Do not edit below this line
