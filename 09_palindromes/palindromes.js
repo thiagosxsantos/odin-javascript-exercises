dff37cb158e0df0a8e8243b99ed70e946ed3ab02
Thiago S. Santos <thiagos.xsantos@gmail.com>
2022-10-15 19:17:37 -0300

    Resolve JS exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  const aux = text.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+  return aux === aux.split('').reverse().join('')
 };
 
 // Do not edit below this line
