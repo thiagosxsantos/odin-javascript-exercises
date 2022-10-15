0c4b4c27159a66b11d56e5086dda9da70cd20708
Thiago S. Santos <thiagos.xsantos@gmail.com>
2022-10-15 18:35:49 -0300

    Resolve JS exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+  const toRemove = Array.prototype.slice.call(arguments, 1);
+  return array.filter((element) => !toRemove.includes(element));
 };
 
 // Do not edit below this line
