9c041a33e5af75bff3f0d2662c7d8b46dc4067d6
Thiago S. Santos <thiagos.xsantos@gmail.com>
2022-10-15 20:04:41 -0300

    Resolve JS exercise snakeCase

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,8 @@
-const snakeCase = function() {
-
+const snakeCase = function(text) {
+  return text.replace(/([A-Z][a-zA-Z0-9]*)/g, ' $1')
+    .toLowerCase()
+    .replace(/(^[^a-z0-9]+|[^a-z0-9]+$)/g, '')
+    .replace(/[^a-z0-9]+/g, '_')
 };
 
 // Do not edit below this line
