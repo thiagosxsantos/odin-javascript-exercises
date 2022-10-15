60cf9a301ed22edbd47fc16c69a3a477bba9e5ba
Thiago S. Santos <thiagos.xsantos@gmail.com>
2022-10-15 18:46:43 -0300

    Resolve JS exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (start < 0 || end < 0 || typeof start !== 'number'|| typeof end !== 'number') {
+    return 'ERROR';
+  }
 
+  if (end < start) {
+    let aux = end;
+    end = start;
+    start = aux;
+  }
+
+  return Array.from({length: 1 + end - start}, (v, k) => k + start)
+    .reduce((sum, a) => sum + a);
 };
 
 // Do not edit below this line
