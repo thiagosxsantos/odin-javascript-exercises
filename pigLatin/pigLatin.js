f1f83f7955499a3b98b5e879d4b46ecba44563c7
Thiago S. Santos <thiagos.xsantos@gmail.com>
2022-10-15 20:28:23 -0300

    Resolve JS exercise pigLatin

--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,5 +1,5 @@
-function pigLatin(string) {
-
+function pigLatin(text) {
+  return text.replace(/([aeiouy][a-z]+)|(([qwrtpsdfghjklzxcvbnm]+u?)([a-z]+))/gi, '$1$4$3ay')
 };
   
 // Do not edit below this line
