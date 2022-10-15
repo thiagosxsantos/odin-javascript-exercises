c9e24d13e885a3d05d5dd5ee072728c31f58eb7b
Thiago S. Santos <thiagos.xsantos@gmail.com>
2022-10-15 19:10:04 -0300

    Resolve JS exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
+}
+
+const sum = function(x) {
+	return x.length > 0 ? x.reduce((sum, x) => sum + x) : 0;
 };
 
-const sum = function() {
-	
+const multiply = function(x, y) {
+  return x.reduce((sum, x) => sum * x);
 };
 
-const multiply = function() {
-
+const power = function(x, y) {
+	return x ** y;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+	return n <= 0 ? 1 : n * factorial(n - 1);
 };
 
 // Do not edit below this line
