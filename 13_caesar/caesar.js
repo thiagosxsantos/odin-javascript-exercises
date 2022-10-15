040e3e15cf92b87a9e9b83aa448dbee66f5f4159
Thiago S. Santos <thiagos.xsantos@gmail.com>
2022-10-15 19:51:57 -0300

    Resolve JS exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,18 @@
-const caesar = function() {
-
+const shiftChar = function(code, base, shifts) {
+  return String.fromCharCode(((code - base + shifts) % 26) + base);
+}
+const caesar = function(text, shifts) {
+  if (shifts < 0 ) shifts = 26 + (shifts % 26);
+  
+  return text.split('').map((c) => {
+    if (c.match(/[A-Z]/)) {
+      return shiftChar(c.charCodeAt(), 65, shifts);
+    } else if (c.match(/[a-z]/)) {
+      return shiftChar(c.charCodeAt(), 97, shifts);
+    } else {
+      return c;
+    }
+  }).join('');
 };
 
 // Do not edit below this line
