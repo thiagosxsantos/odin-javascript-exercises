e2471f5c2079dcbc3978432ad8f619e6e795d586
Thiago S. Santos <thiagos.xsantos@gmail.com>
2022-10-15 19:24:21 -0300

    Resolve JS exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (typeof n === 'string') n = Number(n);
+  if (n < 0) return 'OOPS';
+  if (n === 1 || n === 2) return 1;
+  let n1 = 1;
+  let n2 = 1;
+  let aux;
+  for(let i = 3; i <= n; i++) {
+    aux = n2;
+    n2 = n1 + n2;
+    n1 = aux;
+  }
 
-};
+  return n2;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
