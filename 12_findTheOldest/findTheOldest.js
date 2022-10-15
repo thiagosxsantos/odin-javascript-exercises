d642e15127e6680b33b00414791d20ff9d973c26
Thiago S. Santos <thiagos.xsantos@gmail.com>
2022-10-15 19:35:46 -0300

    Resolve JS exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
+const calcAge = function (person) {
+  return (typeof person.yearOfDeath === 'number' ? person.yearOfDeath : (new Date()).getFullYear()) - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) =>  calcAge(oldest) < calcAge(person) ? person : oldest);
 };
 
 // Do not edit below this line
