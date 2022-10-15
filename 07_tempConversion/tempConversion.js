9fbc4517d2e01a3e3c38b877a80d28d9babcd06b
Thiago S. Santos <thiagos.xsantos@gmail.com>
2022-10-15 19:01:53 -0300

    Resolve JS exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
+const round = function(x) {
+  return Math.round(x * 10) / 10;
+}
 
+const ftoc = function(x) {
+  return round((x - 32) * 5/9);
 };
 
-const ctof = function() {
-
+const ctof = function(x) {
+  return round(x * 9/5 + 32);
 };
 
 // Do not edit below this line
